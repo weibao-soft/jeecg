b2968df2af5cf2721b340ade165bc062106a7da2
kyoliutao12 <13826300503@139.com>
2019-11-14 20:33:04 +0800

    核保接口

--- src/main/java/com/weibao/chaopei/service/GuorenApiService.java
+++ src/main/java/com/weibao/chaopei/service/GuorenApiService.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.jeecgframework.core.common.service.impl.CommonServiceImpl;
 import org.jeecgframework.core.util.DateUtils;
 import org.jeecgframework.web.system.util.HttpClientUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.weibao.chaopei.entity.PolicyEntity;
+import com.weibao.chaopei.entity.ProductDetailEntity;
+import com.weibao.chaopei.entity.ProductEntity;
 import com.weibao.chaopei.util.http.ApiPackage;
 import com.weibao.chaopei.util.http.Body;
 import com.weibao.chaopei.util.http.Element;
@@ -24,103 +27,144 @@ import com.weibao.chaopei.util.http.RelFieldName;
 
 @Service("guorenApiService")
 @Transactional
-public class GuorenApiService {
+public class GuorenApiService extends CommonServiceImpl  {
 	@Autowired
 	GuorenApiConfig apiConfig;
 	
 	Gson gson = new GsonBuilder().create();	
 	/**
 	 * 核保接口，根据传入的保单List，组装成国任核保接口要求的数据，调用核保接口，然后解析得到投保单号、订单号，写回policyEntityList
-	 * @param policyEntityList
+	 * @param policyEntityList 保单list
+	 * @return result list里面是一个map对象，map对象包含：保单id、投保单号、订单号
 	 */
-	public void insuredService(List<PolicyEntity> policyEntityList) {
+	public List<Map<String, String>> insuredService(List<PolicyEntity> policyEntityList) {
+		ProductDetailEntity detail = null;
+		ProductEntity prod = null;
+		List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+		
+		if(policyEntityList != null && policyEntityList.size() > 0) {
+			PolicyEntity entity = policyEntityList.get(0);
+			detail = super.getEntity(ProductDetailEntity.class, entity.getPlanId());
+			prod = super.getEntity(ProductEntity.class, entity.getProdId());
+		}
 		
 		for (PolicyEntity policyEntity : policyEntityList) {
-			ApiPackage package_ = new ApiPackage();
-	    	Head head = new Head();
-	    	head.setCLIENT_ENCODE(apiConfig.CLIENT_ENCODE);
-	    	head.setSERVE_CODE(apiConfig.SERVE_CODE);    	
-	    	head.setCHANNEL_CODE(apiConfig.CHANNEL_CODE);
-	    	head.setINTERFACE_CODE(apiConfig.INTERFACE_CODE_HEBAO);
-	    	head.setINTERFACE_USER_CODE(apiConfig.INTERFACE_USER_CODE);
-	    	head.setINTERFACE_PWD(apiConfig.INTERFACE_PWD);
-	    	head.setBUSINESS_UUID(policyEntity.getId());
-	    	Date now = new Date();	    	
-	    	head.setREQUEST_TIME(DateUtils.datetimeFormatSSSS(now));
-	    	head.setIFTEST(apiConfig.IFTEST);
-	    	head.setXML_LIST_SUFFIX("");
-	    	
-	    	package_.setHEAD(head);
-	    	Body body = new Body();
-	    	package_.setBODY(body);
-	    	body.setOperCode(policyEntity.getPolicyMobile());//填录单人员联系方式
-	    	body.setOperType(apiConfig.operType);
-	    	body.setOrder_source(apiConfig.order_source);
-	    	body.setPermium(policyEntity.getPremium().toString());
-	    	body.setPlan_code(policyEntity.getPlanId());//TODO
-	    	body.setPlan_name(policyEntity.getPlanId());//TODO
-	    	body.setGood_code(policyEntity.getProdId());
-	    	body.setGood_name("国任升华茂林物流责任险");
-	    	body.setAppnum("1");
-	    	
-	    	Element base = new Element();
-	    	body.setBase(base);
-	    	base.setProdCode("1531");
-	    	base.setRelTbl("ins_appl_main");
-	    	RelFieldName relFieldName_base = new RelFieldName();
-	    	base.setRelFieldName(relFieldName_base);
-	    	relFieldName_base.setPlcy_bgn_time("2019-11-30");
-	    	relFieldName_base.setPlcy_end_time("2019-11-29");
-	    	relFieldName_base.setCall_back_url("http://yundmz.pcic:90/jcx-host/guoren/guorenrecvData.ashx");
-	    	
-	    	Element applicant = new Element();
-	    	body.setApplicant(applicant);
-	    	applicant.setProdCode("1531");
-	    	applicant.setRelTbl("ins_appl_applnt");
-	    	RelFieldName relFieldName_applicant = new RelFieldName();
-	    	applicant.setRelFieldName(relFieldName_applicant);
-	    	relFieldName_applicant.setAppl_name("东莞市大朗超捷货运服务部");
-	    	relFieldName_applicant.setClnt_type("2");
-	    	relFieldName_applicant.setCertf_type("99");
-	    	relFieldName_applicant.setCertf_no("92441900L06692572F");
-	    	relFieldName_applicant.setEmail("");
-	    	relFieldName_applicant.setMobile("13826300503");
-	    	relFieldName_applicant.setBank_account("");
-	    	relFieldName_applicant.setBank_account_name("");
-	    	relFieldName_applicant.setBank_code("");
-	    	
-	    	Element insured = new Element();
-	    	body.setInsured(insured);
-	    	insured.setProdCode("1531");
-	    	insured.setRelTbl("ins_appl_insrnt");
-	    	RelFieldName relFieldName_insured = new RelFieldName();
-	    	insured.setRelFieldName(relFieldName_insured);    	
-	    	relFieldName_insured.setCertf_type("99");
-	    	relFieldName_insured.setCertf_no("92441900L06692572F");
-	    	relFieldName_insured.setInsrnt_name("东莞市大朗超捷货运服务部");
-	    	relFieldName_insured.setMobile("13826300503");
-	    	
-	    	Element tgtCar = new Element();
-	    	body.setTgtCar(tgtCar);
-	    	tgtCar.setProdCode("1531");
-	    	tgtCar.setRelTbl("ins_appl_tgt_car");
-	    	RelFieldName relFieldName_tgtCar = new RelFieldName();
-	    	tgtCar.setRelFieldName(relFieldName_tgtCar);    	
-	    	relFieldName_tgtCar.setLicense_no("粤S98684");
-	    	relFieldName_tgtCar.setFrame_no("LGGR2BG42CL714501");
-	    	relFieldName_tgtCar.setEngine_no("J64L1C00023");
-	    	relFieldName_tgtCar.setSeat_num("5");
-	    	relFieldName_tgtCar.setToncount("4.5");
-	    	
-	    	List limit = new ArrayList();
-	    	body.setLimit(limit);
-	    	
-	    	Map package_map = new HashMap();
-	    	package_map.put("PACKAGE", package_);
-	    	String json = gson.toJson(package_map);
-	    	
-	    	String response = HttpClientUtil.httpPostRequest(apiConfig.API_URL, json);
-			
+			try {
+				ApiPackage package_ = new ApiPackage();
+		    	Head head = new Head();
+		    	head.setCLIENT_ENCODE(apiConfig.CLIENT_ENCODE);
+		    	head.setSERVE_CODE(apiConfig.SERVE_CODE);    	
+		    	head.setCHANNEL_CODE(apiConfig.CHANNEL_CODE);
+		    	head.setINTERFACE_CODE(apiConfig.INTERFACE_CODE_HEBAO);
+		    	head.setINTERFACE_USER_CODE(apiConfig.INTERFACE_USER_CODE);
+		    	head.setINTERFACE_PWD(apiConfig.INTERFACE_PWD);
+		    	head.setBUSINESS_UUID(policyEntity.getId());
+		    	Date now = new Date();	    	
+		    	head.setREQUEST_TIME(DateUtils.datetimeFormatSSSS(now));
+		    	head.setIFTEST(apiConfig.IFTEST);
+		    	head.setXML_LIST_SUFFIX("");
+		    	
+		    	package_.setHEAD(head);
+		    	Body body = new Body();
+		    	package_.setBODY(body);
+		    	body.setOperCode(policyEntity.getPolicyMobile());//填录单人员联系方式
+		    	body.setOperType(apiConfig.operType);
+		    	body.setOrder_source(apiConfig.order_source);
+		    	body.setPermium(policyEntity.getPremium().toString());
+		    	body.setPlan_code(detail.getPlanCode());
+		    	body.setPlan_name(detail.getPlanName());
+		    	body.setGood_code(prod.getGoodCode());
+		    	body.setGood_name(prod.getProdName());
+		    	body.setAppnum(apiConfig.appnum);
+		    	
+		    	Element base = new Element();
+		    	body.setBase(base);
+		    	base.setProdCode(prod.getProdCode());
+		    	base.setRelTbl(apiConfig.ins_reltab);
+		    	base.setRelTbl(apiConfig.base_reltab);
+		    	RelFieldName relFieldName_base = new RelFieldName();
+		    	base.setRelFieldName(relFieldName_base);
+		    	relFieldName_base.setPlcy_bgn_time(DateUtils.formatDate(policyEntity.getStartDate()));
+		    	relFieldName_base.setPlcy_end_time(DateUtils.formatDate(policyEntity.getEndDate()));
+		    	relFieldName_base.setCall_back_url(apiConfig.call_back_url);
+		    	
+		    	Element applicant = new Element();
+		    	body.setApplicant(applicant);
+		    	applicant.setProdCode(prod.getProdCode());
+		    	applicant.setRelTbl(apiConfig.appl_reltab);
+		    	RelFieldName relFieldName_applicant = new RelFieldName();
+		    	applicant.setRelFieldName(relFieldName_applicant);
+		    	relFieldName_applicant.setAppl_name(policyEntity.getHolderCompName());
+		    	relFieldName_applicant.setClnt_type("2");
+		    	relFieldName_applicant.setCertf_type("99");
+		    	relFieldName_applicant.setCertf_no(policyEntity.getHolderOrgCode());
+		    	relFieldName_applicant.setEmail("");
+		    	relFieldName_applicant.setMobile(policyEntity.getPolicyMobile());
+		    	relFieldName_applicant.setBank_account("");
+		    	relFieldName_applicant.setBank_account_name("");
+		    	relFieldName_applicant.setBank_code("");
+		    	
+		    	Element insured = new Element();
+		    	body.setInsured(insured);
+		    	insured.setProdCode(prod.getProdCode());
+		    	insured.setRelTbl(apiConfig.ins_reltab);
+		    	RelFieldName relFieldName_insured = new RelFieldName();
+		    	insured.setRelFieldName(relFieldName_insured);    	
+		    	relFieldName_insured.setCertf_type("99");
+		    	relFieldName_insured.setCertf_no(policyEntity.getInsuredCompName());
+		    	relFieldName_insured.setInsrnt_name(policyEntity.getInsuredOrgCode());
+		    	relFieldName_insured.setMobile(policyEntity.getPolicyMobile());
+		    	
+		    	Element tgtCar = new Element();
+		    	body.setTgtCar(tgtCar);
+		    	tgtCar.setProdCode(prod.getProdCode());
+		    	tgtCar.setRelTbl(apiConfig.tgt_reltab);
+		    	RelFieldName relFieldName_tgtCar = new RelFieldName();
+		    	tgtCar.setRelFieldName(relFieldName_tgtCar);    	
+		    	relFieldName_tgtCar.setLicense_no(policyEntity.getPlateNo());
+		    	relFieldName_tgtCar.setFrame_no(policyEntity.getFrameNo());
+		    	relFieldName_tgtCar.setEngine_no(policyEntity.getEngineNo());
+		    	relFieldName_tgtCar.setSeat_num("5");
+		    	relFieldName_tgtCar.setToncount("4.5");	    	
+		    	List limit = new ArrayList();
+		    	body.setLimit(limit);
+		    	
+		    	Map package_map = new HashMap();
+		    	package_map.put("PACKAGE", package_);
+		    	String json = gson.toJson(package_map);	    
+		    	try {
+			    	String response = HttpClientUtil.httpPostRequest(apiConfig.API_URL, json);	
+			    	Map mapRes = gson.fromJson(response, Map.class);
+			    	if(mapRes.get("RESPONSE_BODY") != null) {
+			    		Map resBody = (Map)mapRes.get("RESPONSE_BODY");
+			    		Map resData = (Map)resBody.get("data");
+			    		
+			    		String updSql = "update wb_insurance_product set proposal_no=?, order_no=? where id=?";
+			    		
+			    		String proposalNo = (String)resData.get("proposalNo");
+			    		String orderNo = (String)resData.get("orderNo");	    		
+			    		int updCnt = super.executeSql(updSql, proposalNo, orderNo, policyEntity.getId());
+			    		if(updCnt > 0) {
+			    			//
+				    		Map<String, String> r1 = new HashMap<String, String>();
+				    		r1.put("id", policyEntity.getId());
+				    		r1.put("proposalNo", proposalNo);
+				    		r1.put("orderNo", orderNo);
+				    		result.add(r1);
+			    		}else {
+			    			
+			    		}
+			    	}else {
+			    		//接口返回的数据里没有RESPONSE_BODY
+			    	}
+		    	}catch(Exception e) {
+		    		//调用远程接口或者解析接口出错，该保单不列入核保单
+		    	}
+			}catch(Exception e1) {
+				//构造接口请求数据出错
+			}	    	
 		}
+		
+		return result;
 	}
 }
