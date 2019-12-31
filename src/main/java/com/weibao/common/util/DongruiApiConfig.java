834547f9ab7812a33b5190a52ebe2b52b61b02f6
dengxujin <7058028@qq.com>
2019-12-31 11:23:16 +0800

    邓栩金，修改东瑞支付接口的bug

--- src/main/java/com/weibao/common/util/DongruiApiConfig.java
+++ src/main/java/com/weibao/common/util/DongruiApiConfig.java
@@ -9,15 +9,15 @@ import org.springframework.stereotype.Component;
 public class DongruiApiConfig {
 	//md5加密Key
 	@Value("${MD5_KEY}")
-	public String MD5_KEY = "83JD89UER-WEIBAO";
+	public String MD5_KEY;
 	//支付加密Key
 	@Value("${PAY_AES_KEY}")
-	public String PAY_AES_KEY = "EF6AD27F389B01A247CD5A12F8E08EA5";
+	public String PAY_AES_KEY;
 	//退款加密Key
 	@Value("${REFUND_AES_KEY}")
-	public String REFUND_AES_KEY = "EO6AD27F389X01A247CD5A12F8E021K1";
+	public String REFUND_AES_KEY;
 	//账号
 	@Value("${WEIBAO}")
-	public String WEIBAO = "WEIBAO";
+	public String WEIBAO;
 
 }
